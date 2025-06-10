8c9f038622071c4ce151e6ef34c7bfbc85f818d3
marquezinejeanrocha <20231835@novaims.unl.pt>
2025-06-10 11:42:49 +0100

    Possible division

---
+++ app/src/main/java/pt/ola/online_classes_app/student/student_class_schedule.kt
@@ -0,0 +1,21 @@
+package pt.ola.online_classes_app.student
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import pt.ola.online_classes_app.R
+
+class student_class_schedule : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_student_class_schedule)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
