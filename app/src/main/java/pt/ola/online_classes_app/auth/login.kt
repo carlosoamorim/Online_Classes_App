a6f9dfe0b7eead4b8edfdd341f272087b7e351f3
marquezinejeanrocha <20231835@novaims.unl.pt>
2025-06-16 14:50:21 +0100

    Login

--- app/src/main/java/pt/ola/online_classes_app/auth/login.kt
+++ app/src/main/java/pt/ola/online_classes_app/auth/login.kt
@@ -1,21 +1,26 @@
 package pt.ola.online_classes_app.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pt.ola.online_classes_app.R
+import pt.ola.online_classes_app.professor.professor_class_schedule
+import pt.ola.online_classes_app.professor.professor_dashboard
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val loginButton = findViewById<Button>(R.id.login_btn)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, professor_dashboard::class.java)
+            startActivity(intent)
         }
     }
 }
