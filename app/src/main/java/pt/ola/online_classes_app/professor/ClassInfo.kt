2a0a3f31a813d2193c98190af0b5b687a6ab0f7e
MikhailGazizov <michaelgylesaces@gmail.com>
2025-06-17 03:15:25 +0100

    Log In connectivity

--- app/src/main/java/pt/ola/online_classes_app/professor/ClassInfo.kt
+++ app/src/main/java/pt/ola/online_classes_app/professor/ClassInfo.kt
@@ -1,8 +1,12 @@
 package pt.ola.online_classes_app.professor
+import pt.ola.online_classes_app.model.Subject
 
-data class ClassInfo (
-    val courseName: String,
-    val classTime: String,
-    val classRoom: String,
-    val teacherId: Int
+
+data class ClassInfo(
+    val id: Int,
+    val subject_id: Int,
+    val auditorium: String,
+    val start_time: String,
+    val end_time: String,
+    val subject_name: String
 )
